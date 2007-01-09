fa501059b6aad760bc6f55683f789a6c23373ac4
zeflunk <zeflunk@59a0dfbf-8f1f-0410-974c-09cbed490068>
2007-01-09 20:44:18 +0000

    Adding new files to the last mega-checkin

---
+++ trunk/lib/DbusManager.cc
@@ -0,0 +1,86 @@
+/*
+Copyright (C) 2006  Christian Lundgren
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "linkage/DbusManager.hh"
+
+/* FIXME: This is sooo ugly... */
+#include "linkage/Engine.hh"
+
+void DbusManager::callback_handler(int action, const char* data)
+{
+	switch (action)
+	{
+		case ACTION_QUIT:
+			Engine::instance()->get_dbus_manager()->signal_quit().emit();
+			break;
+		case ACTION_OPEN:
+			Engine::instance()->get_dbus_manager()->signal_open().emit(data);
+			break;
+		case ACTION_TOGGLE_VISIBLE:
+			Engine::instance()->get_dbus_manager()->signal_toggle_visible().emit();
+			break;
+	}
+}
+
+Glib::RefPtr<DbusManager> DbusManager::create()
+{
+	return Glib::RefPtr<DbusManager>(new DbusManager());
+}
+
+DbusManager::DbusManager() : RefCounter<DbusManager>::RefCounter(this)
+{
+	int p;
+	m_connection = init(&p, &DbusManager::callback_handler);
+	primary = (bool)p;
+}
+
+
+DbusManager::~DbusManager()
+{
+	std::cout << "DM destructor\n";
+}
+
+void DbusManager::send(const Glib::ustring& interface, const Glib::ustring& msg)
+{
+	c_send(m_connection, interface.c_str(), msg.c_str());
+}
+
+void DbusManager::send(const Glib::ustring& interface)
+{
+	c_send(m_connection, interface.c_str(), NULL);
+}
+
+bool DbusManager::is_primary()
+{
+  return primary;
+}
+
+sigc::signal<void> DbusManager::signal_quit()
+{
+  return m_signal_quit;
+}
+
+sigc::signal<void, const Glib::ustring&> DbusManager::signal_open()
+{
+  return m_signal_open;
+}
+
+sigc::signal<void> DbusManager::signal_toggle_visible()
+{
+  return m_signal_toggle_visible;
+}
---
+++ trunk/lib/linkage/dbus-c.h
@@ -0,0 +1,27 @@
+/*
+Copyright (C) 2006  Christian Lundgren
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <glib.h>
+#include <dbus/dbus-glib.h>
+#include <dbus/dbus-protocol.h>
+
+enum DbusAction { ACTION_QUIT, ACTION_OPEN, ACTION_TOGGLE_VISIBLE };
+
+DBusGConnection* init(int* p, void (*cb_func)(int, const char*));
+DBusHandlerResult signal_filter(DBusGConnection *connection, DBusGMessage *message, void *user_data);
+void c_send(DBusGConnection *connection, const char* interface, const char* msg);
