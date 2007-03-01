5ce3dbccdd62f52fdc5b648c1cbb821a394c6591
zeflunk <zeflunk@59a0dfbf-8f1f-0410-974c-09cbed490068>
2007-03-01 16:11:41 +0000

    Now using config.h, also fixed user-agent/fingerprint

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -3,4 +3,4 @@
 rev=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%i\n", $2}'`
 sed -e "s/@REVISION@/${rev}/g" < "configure.ac.in" > "configure.ac"
 
-libtoolize --force && aclocal && automake --add-missing --copy && autoconf
+libtoolize --force && aclocal && autoheader && automake --add-missing --copy && autoconf
