36b8521209a6c409fc28e13f76ccd9e77c9d14fc
zeflunk <zeflunk@59a0dfbf-8f1f-0410-974c-09cbed490068>
2007-04-02 21:04:15 +0000

    Tracker responses handled better

--- trunk/lib/linkage/TorrentManager.hh
+++ trunk/lib/linkage/TorrentManager.hh
@@ -39,7 +39,8 @@ private:
 
 	sigc::signal<void, const sha1_hash&, const Glib::ustring&, unsigned int> m_signal_added;
 	sigc::signal<void, const sha1_hash&> m_signal_removed;
-	
+
+	void on_tracker_announce(const sha1_hash& hash, const Glib::ustring& msg);
 	void on_tracker_reply(const sha1_hash& hash, const Glib::ustring& reply, int peers);
 	void on_tracker_warning(const sha1_hash& hash, const Glib::ustring& reply);
 	void on_tracker_failed(const sha1_hash& hash, const Glib::ustring& reply, int code, int times);
