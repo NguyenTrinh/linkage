36bd4d6a842d53c31461c7b5cf3e5a3a221b0af8
ZeFlunk <ZeFlunk@59a0dfbf-8f1f-0410-974c-09cbed490068>
2007-07-23 16:59:01 +0000

    fixed build errors

--- lib/PluginManager.cc
+++ lib/PluginManager.cc
@@ -16,6 +16,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA	02110-1301, USA.
 */
 
+#include "config.h"
+
 #include "linkage/PluginManager.hh"
 #include "linkage/Engine.hh"
 
--- lib/TorrentManager.cc
+++ lib/TorrentManager.cc
@@ -17,9 +17,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA	02110-1301, USA.
 */
 
 #include <vector>
-#include <iostream>
 
 #include "linkage/Engine.hh"
+#include "linkage/Utils.hh"
 #include "linkage/TorrentManager.hh"
 
 Glib::RefPtr<TorrentManager> TorrentManager::create()
