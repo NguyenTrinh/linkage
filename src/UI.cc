f44c07ee2508120fc0f883c147589e7a85c45977
ZeFlunk <ZeFlunk@59a0dfbf-8f1f-0410-974c-09cbed490068>
2007-08-19 20:02:27 +0000

    use single line mode instead of replacing line breaks manually

--- src/UI.cc
+++ src/UI.cc
@@ -317,6 +317,8 @@ UI::UI(BaseObjectType* cobject, const Glib::RefPtr<Gnome::Glade::Xml>& refGlade)
 		max_down = 1000;
 	spinbutton_down->set_range(0, max_down);
 
+	label_comment->set_single_line_mode(!sm->get_bool("ui/allow_linebreak_comments"));
+
 	Engine::get_settings_manager()->signal_update_settings().connect(sigc::mem_fun(this, &UI::on_settings));
 
 	Engine::get_session_manager()->signal_invalid_bencoding().connect(sigc::mem_fun(this, &UI::on_invalid_bencoding));
@@ -591,25 +593,8 @@ void UI::update_statics(const WeakPtr<Torrent>& torrent)
 	libtorrent::torrent_info info = torrent->get_info();
 
 	label_creator->set_text(info.creator());
-	if (Engine::get_settings_manager()->get_bool("ui/allow_linebreak_comments"))
-		label_comment->set_text(info.comment());
-	else
-	{
-		Glib::ustring comment = info.comment();
-		unsigned int pos;
-		while ((pos = comment.find("\n")) != Glib::ustring::npos)
-		{
-			unsigned int len = 1;
-			if (pos > 0 && comment[pos - 1] == '\r')
-			{
-				pos--;
-				len++;
-			}
-			comment = comment.replace(pos, len, " ");
-		}
-		label_comment->set_text(comment);
-		//FIXME: set tooltip to full comment
-	}
+	label_comment->set_text(info.comment());
+	//FIXME: set tooltip to full comment
 
 	if (info.creation_date())
 		label_date->set_text(Glib::ustring(to_simple_string(*info.creation_date())) );
@@ -780,6 +765,8 @@ void UI::on_settings()
 	if (max_down == 0)
 		max_down = 10000;
 	spinbutton_down->set_range(0, max_down);
+	label_comment->set_single_line_mode(
+		!Engine::get_settings_manager()->get_bool("ui/allow_linebreak_comments"));
 }
 
 void UI::on_add()
