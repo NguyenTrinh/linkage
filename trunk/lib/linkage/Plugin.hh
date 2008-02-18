94a0038b145b5d432acf723d49bc0fd4ad792b05
ZeFlunk <ZeFlunk@59a0dfbf-8f1f-0410-974c-09cbed490068>
2008-02-18 14:43:26 +0000

    * src/SettingsWin.cc: * lib/linkage/Plugin.hh: Plugins should now return a config dialog 	instead of just a config widget.

--- trunk/lib/linkage/Plugin.hh
+++ trunk/lib/linkage/Plugin.hh
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA	02110-1301, USA.
 
 namespace Gtk
 {
-class Widget;
+class Dialog;
 }
 
 namespace Linkage
@@ -74,21 +74,17 @@ public:
 	sigc::signal<void, Plugin*> signal_unloading();
 
 	/*
-		Do _not_ override with "return plugin_info()", since that function might
-		be bound to another plugin. Should return an equal struct of plugin_info()
-	*/
+	 * Do _not_ override with "return plugin_info()", since that function might
+	 * be bound to another plugin. Should return an equal struct of plugin_info()
+	 */
 	virtual Plugin::Info get_info();
 
 	/* This can be used to pass data to another plugin or whatever */
 	/* FIXME: Use dbus manager as well, allow plugin to register signals/interfaces */
 	virtual gpointer get_user_data(gpointer arg = NULL) { return NULL; }
 
-	/*
-		Override and return a widget containing your plugins options,
-		Use Gtk::Widget::signal_parent_changed() to be notified when the configure
-		window is closed, the widget should be deleted by the plugin.
-	*/
-	virtual Gtk::Widget* get_config_widget() { return NULL; }
+	/* Override and return a dialog for configuring you plugin */
+	virtual Gtk::Dialog* get_config_dialog() { return NULL; }
 
 	Plugin();
 	virtual ~Plugin();
