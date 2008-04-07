6da0891f81cd9e78cc51a49233afe98566107702
ZeFlunk <ZeFlunk@59a0dfbf-8f1f-0410-974c-09cbed490068>
2008-04-07 15:17:20 +0000

    * src/FileList.cc: included definition for std::accumulate 	* src/UI.cc: fixed ambigious suffix_value call 	* src/TorrentList.cc: translated strings in edit columns dialog 	* po/sv.po: updated swedish translation 	* po/POTFILES.in: added source files for translations

--- trunk/src/FileList.cc
+++ trunk/src/FileList.cc
@@ -17,6 +17,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA	02110-1301, USA.
 */
 
 #include <vector>
+#include <numeric>
 
 #include <gtkmm/icontheme.h>
 #include <gtkmm/separatormenuitem.h>
