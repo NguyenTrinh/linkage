369b0b4573d1a7364b588cbbc361ce589cec462b
ZeFlunk <ZeFlunk@59a0dfbf-8f1f-0410-974c-09cbed490068>
2008-04-15 01:46:13 +0000

    0.2.0 release tag

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,5 @@
 #! /bin/sh
 
-rev=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%i\n", $2}'`
-sed -e "s/@REVISION@/${rev}/g" < "configure.ac.in" > "configure.ac"
-
 intltoolize --force --copy && \
 libtoolize --force && \
 aclocal -I m4 && \
